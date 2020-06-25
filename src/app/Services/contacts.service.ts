4eb87d57224e327d6cfbacc360d08179efd4f01e
shani <shaniromanov@gmail.com>
2020-06-26 00:40:50 +0300

    add contact without style

--- src/app/Services/contacts.service.ts
+++ src/app/Services/contacts.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Group } from '../DTO/group';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactsService {
-
+groups:Array<Group>=[new Group("Family"),new Group("Freind")]
   constructor() { }
 }
--- src/app/add-contact/add-contact.component.ts
+++ src/app/add-contact/add-contact.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
 import { MeansOfContact } from '../DTO/means-of-contact';
 import { Email } from '../DTO/email';
 import { PhoneNumber } from '../DTO/phone-number';
+import { ContactsService } from '../Services/contacts.service';
+import { Group } from '../DTO/group';
 
 @Component({
   selector: 'app-add-contact',
@@ -12,10 +14,14 @@ import { PhoneNumber } from '../DTO/phone-number';
 export class AddContactComponent implements OnInit {
   form:FormGroup
   subForm:FormGroup=null
+  groupList:Array<String>
  
   meansContact:{[meanType:string]:typeof  MeansOfContact}={"Email":Email,"Phone Number":PhoneNumber}
   get MeansContact(): FormArray {
     return this.form.get('MeansContact') as FormArray;
+  }
+  get Groups(): FormArray {
+    return this.form.get('Groups') as FormArray;
   }
   onSubmit(empForm: any, event: Event){
     event.preventDefault();
@@ -30,19 +36,28 @@ export class AddContactComponent implements OnInit {
       
       
   }
+  AddGroupToForm(){
+    var f:FormArray = this.form.get('Groups') as FormArray
+      f.push(new FormControl())
+  }
   keys() : Array<string> {
     return Object.keys(this.meansContact);
   }
   ngOnInit(): void {
+    this.groupList=this.contactsService.groups.map(v=>v.groupName)
       this.form = new FormGroup({
-        MeansContact:new FormArray([])
+        FirstName:new FormControl(),
+        LastName:new FormControl(),
+        Adress:new FormControl(),
+        MeansContact:new FormArray([]),
+        Groups:new FormArray([])
       })
 
   }
 //   public setParentControl(formGroup: FormGroup) {
 //         this.subForm=formGroup
 // }
-  constructor() { }
+  constructor(private contactsService:ContactsService) { }
 
 
 }
